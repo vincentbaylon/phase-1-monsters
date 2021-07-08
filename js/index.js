6d199d32fd68130b594faed504e8576e2ee7b061
vincentbaylon <vincentbaylon@gmail.com>
2021-07-07 17:17:38 -0700

    Refactored code

--- js/index.js
+++ js/index.js
@@ -1,28 +1,12 @@
 function fetchMonsters() {
     fetch('http://localhost:3000/monsters/?_limit=50')
     .then(res => res.json())
-    .then(json => renderMonster(json))
+    .then(json => json.forEach(renderMonster))
 }
 
 fetchMonsters()
 
-function renderMonster(monster) {
-    monster.forEach(eachMonster => {
-        let li = document.createElement('li')
-        let h1Name = document.createElement('h1')
-        let pAge = document.createElement('p')
-        let pDesc = document.createElement('p')
-
-        h1Name.textContent = eachMonster.name
-        pAge.textContent = `Age: ${eachMonster.age}`
-        pDesc.textContent = `Desc: ${eachMonster.description}`
-        
-        li.append(h1Name, pAge, pDesc)
-        document.querySelector('#monster-container').append(li)
-    })
-}
-
-function renderOne(monster) {
+function renderMonster(monster) { 
     let li = document.createElement('li')
     let h1Name = document.createElement('h1')
     let pAge = document.createElement('p')
@@ -44,6 +28,6 @@ document.querySelector('#monster-form').addEventListener('submit', (e) => {
         age: e.target.age.value,
         description: e.target.desc.value
     }
-    
-    renderOne(newMonster)
+
+    renderMonster(newMonster)
 })
